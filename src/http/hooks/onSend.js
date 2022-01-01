b034218f3406bb78160686c25f640d97c3b0b5e0
somebeaver <admin@somebeaver.ca>
2021-12-31 21:28:24 -0500

    add CORS

--- src/http/hooks/onSend.js
+++ src/http/hooks/onSend.js
@@ -5,5 +5,9 @@
  */
 module.exports = (request, response, payload, next) => {
   response.header('cardinal-server-application', 1)
+  
+  // TODO: Should this be kept?
+  response.header('Access-Control-Allow-Origin', '*')
+
   next()
 }
